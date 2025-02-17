ac5135441aaea37b3814fc432cffeb4c6f77845e
pablohernandezm <phernandezm07@gmail.com>
2025-02-17 17:24:51 -0500

    feat: steam-run desktop entry

--- hosts/desktop/settings/gaming/default.nix
+++ hosts/desktop/settings/gaming/default.nix
@@ -6,4 +6,20 @@
     lutris
     prismlauncher
   ];
+
+  xdg.desktopEntries.steam-run = {
+    name = "Steam Run";
+    exec = "steam-run %f";
+    icon = "steam";
+    type = "Application";
+    mimeType = [ "application/x-shellscript" ];
+    terminal = false;
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/x-shellscript" = "steam-run.desktop";
+    };
+  };
 }
