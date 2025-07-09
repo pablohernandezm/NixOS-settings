c73f2d4fa751778bc9e92ce5861bc508088167f7
pablohernandezm <phernandezm07@gmail.com>
2025-07-08 19:34:17 -0500

    fix(theming): main + icons

--- hosts/desktop/settings/themes/default.nix
+++ hosts/desktop/settings/themes/default.nix
@@ -1,34 +1,49 @@
-{ pkgs, ... }:
-let
-  themePackage = pkgs.rose-pine-gtk-theme;
-  themeName = "rose-pine";
-  extraConf = ''
-  '';
-in
-{
-  imports = [
-    ./cursor.nix
-    ./icons.nix
-  ];
+{pkgs, ...}: let
+  themePackage = pkgs.everforest-gtk-theme;
+  themeName = "Everforest-Dark-B";
 
+  iconPackage = pkgs.everforest-gtk-theme;
+  iconName = "Everforest-Dark";
+
+  cursorTheme = "BreezeX-RosePine-Linux";
+  cursorPackage = pkgs.rose-pine-cursor;
+  cursorSize = 24;
+in {
+  # Theme
   gtk = {
     theme = {
       name = themeName;
       package = themePackage;
     };
-
-    gtk2.extraConfig = ''
-      gtk-application-prefer-dark-theme = true;
-    '';
-
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-
-    gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
-    };
   };
+
+  # Icon
+  gtk.iconTheme = {
+    package = iconPackage;
+    name = iconName;
+  };
+
+  services.dunst.iconTheme = {
+    package = iconPackage;
+    name = iconName;
+  };
+
+  # Cursor
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    hyprcursor = {
+      enable = true;
+      size = cursorSize;
+    };
+    package = cursorPackage;
+    name = cursorTheme;
+    size = cursorSize;
+  };
+
+  gtk.enable = true;
+
+  # wayland.windowManager.hyprland.settings.exec-once = [
+  #   "hyprctl setcursor ${cursorTheme} ${toString cursorSize}"
+  # ];
 }
